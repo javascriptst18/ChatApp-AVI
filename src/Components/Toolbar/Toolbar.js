84cb903f4c9d26745cd7d1d42e65e8b15253fcf2
Alan_H <alanhabib.atb@gmail.com>
2018-09-08 18:21:50 +0200

    tried somestuff new in the css

--- src/Components/Toolbar/Toolbar.js
+++ src/Components/Toolbar/Toolbar.js
@@ -3,29 +3,32 @@ import "./Toolbar.css";
 import DrawerToggle from "../SideDrawer/DrawerToggle";
 
 const toolbar = props => (
-  <header className="toolbar">
-    <nav className="toolbar_navigation">
-      <div className="toolbar_banan_button">
-        <DrawerToggle click={props.drawerClickHandler} />
-      </div>
-      {/* <div className="toolbar_logo">
-        <a href="/">
-          <i className="fas fa-home" />
-        </a>
-      </div> */}
-      <div className="spacer" />
-      <div className="toolbar_navigation-items">
-        <ul>
-          <li>
-            <a onClick={props.toggleNotes}>Notes</a>
-          </li>
-          <li>
-            <a onClick={props.toggleChat}>Chatt</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+  //   <header className="toolbar">
+  //     <nav className="toolbar_navigation">
+  //       <div className="toolbar_banan_button">
+  //         <DrawerToggle click={props.drawerClickHandler} />
+  //       </div>
+  //       {/* <div className="toolbar_logo">
+  //         <a href="/">
+  //           <i className="fas fa-home" />
+  //         </a>
+  //       </div> */}
+  //       <div className="spacer" />
+  //       {/* <div className="toolbar_navigation-items">
+  //         <ul>
+  //           <li>
+  //             <a onClick={props.toggleNotes}>Notes</a>
+  //           </li>
+  //           <li>
+  //             <a onClick={props.toggleChat}>Chatt</a>
+  //           </li>
+  //         </ul>
+  //       </div> */}
+  //     </nav>
+  //   </header>
+  <div className="top-wrapper">
+    <DrawerToggle click={props.drawerClickHandler} />
+  </div>
 );
 
 export default toolbar;
